b938f175fa7d2dc16aee12c2ee3c2e85c42c0ec7
ABIZEYIMANA Victor <svicky.shema@gmail.com>
2020-02-21 18:19:53 +0200

    ft(setup): set up new react project
    Config with webpack
    Install Eslint
    
    [finishes #171410833]

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./app.scss";
+
+const Index = () => {
+  return <div>Hello React!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+require("dotenv").config();
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      },
+      {
+        test: /\.(png|woff|woff2|svg|ttf|eot)($|\?)/i,
+        use: {
+          loader: "url-loader?limit=5000"
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      { test: /\.jsx?$/, loader: "babel-loader" }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        API_URL: JSON.stringify(process.env.API_URL)
+      }
+    })
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"]
+  }
+};
